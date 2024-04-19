mwilck/sg3_utils

commit 0f1230a6e4541f4791762bd4eb1478c399d68a90

sg_readcap+sg_luns: add --inhex= and json
Also add --sinq_inraw= to sg_readcap and sg_luns.
Add hex reference files for sg_luns and sg_readcap
in inhex directory for testing plus a
inq_standard.raw in the same directory.



git-svn-id: https://svn.bingwo.ca/repos/sg3_utils/trunk@1001 6180dd3e-e324-4e3e-922d-17de1ae2f315

src/sg_inq.c
@@ -53,7 +53,7 @@
 
 #include "sg_vpd_common.h"  /* for shared VPD page processing with sg_vpd */
 
-static const char * version_str = "2.38 20230129";  /* spc6r07, sbc5r04 */
+static const char * version_str = "2.39 20230202";  /* spc6r07, sbc5r04 */
 
 #define MY_NAME "sg_inq"
 
@@ -2101,26 +2101,29 @@ decode_b3_vpd(uint8_t * buff, int len, struct opts_t * op, sgj_opaque_p jop)
 }
 
 static void
-std_inq_decode(struct opts_t * op, sgj_opaque_p jop, int off)
+std_inq_decode(const uint8_t * rp, int len, struct opts_t * op,
+               sgj_opaque_p jop)
 {
     uint8_t ansi_version;
-    int len, pqual, pdt, k, j;
+    int pqual, pdt, k, j, rsp_len, gv_len;
     sgj_state * jsp = &op->json_st;
     bool as_json = jsp->pr_as_json;
     const char * cp;
-    const uint8_t * rp;
     int vdesc_arr[8];
     char b[128];
     static const int blen = sizeof(b);
 
-    rp = rsp_buff + off;
+    if (len < 4) {
+        pr2serr("%s: len [%d] too short\n", __func__, len);
+        return;
+    }
     memset(vdesc_arr, 0, sizeof(vdesc_arr));
     if (op->do_raw) {
-        dStrRaw((const char *)rp, op->maxlen);
+        dStrRaw((const char *)rp, len);
         return;
     } else if (op->do_hex > 0) {
         /* with -H, print with address, -HH without */
-        hex2stdout(rp, op->maxlen, no_ascii_4hex(op));
+        hex2stdout(rp, len, no_ascii_4hex(op));
         return;
     }
     pqual = (rp[0] & 0xe0) >> 5;
@@ -2138,7 +2141,13 @@ std_inq_decode(struct opts_t * op, sgj_opaque_p jop, int off)
             sgj_pr_hr(jsp, "%s [reserved or vendor specific qualifier "
                       "[%d]]\n", b, pqual);
     }
-    len = rp[4] + 5;
+    gv_len = len;
+    rsp_len = rp[4] + 5;
+    if (op->verbose > 2)
+        pr2serr(">> requested %d bytes, %d bytes available\n", len, rsp_len);
+    if (rsp_len < len)
+        len = rsp_len;
+
     /* N.B. rp[2] full byte is 'version' in SPC-2,3,4 but in SPC
      * [spc-r11a (1997)] bits 6,7: ISO/IEC version; bits 3-5: ECMA
      * version; bits 0-2: SCSI version */
@@ -2176,25 +2185,25 @@ std_inq_decode(struct opts_t * op, sgj_opaque_p jop, int off)
                   !!(rp[7] & 0x20), !!(rp[7] & 0x10), !!(rp[7] & 0x08),
                   !!(rp[7] & 0x04));
         sgj_pr_hr(jsp, "CmdQue=%d\n", !!(rp[7] & 0x02));
-        if (op->maxlen > 56)
+        if (len > 56)
             sgj_pr_hr(jsp, "  [SPI: Clocking=0x%x  QAS=%d  IUS=%d]\n",
                       (rp[56] & 0x0c) >> 2, !!(rp[56] & 0x2),
                       !!(rp[56] & 0x1));
-        if (op->maxlen >= len)
+        if (gv_len >= len)
             sgj_pr_hr(jsp, "    length=%d (0x%x)", len, len);
         else
             sgj_pr_hr(jsp, "    length=%d (0x%x), but only fetched %d bytes",
-                      len, len, op->maxlen);
+                      len, len, gv_len);
         if ((ansi_version >= 2) && (len < SAFE_STD_INQ_RESP_LEN))
             sgj_pr_hr(jsp, "\n  [for SCSI>=2, len>=36 is expected]");
         cp = sg_get_pdt_str(pdt, blen, b);
         if (strlen(cp) > 0)
             sgj_pr_hr(jsp, "   Peripheral device type: %s\n", cp);
     }
-    if (op->maxlen <= 8) {
+    if (len <= 8) {
         if (! op->do_export)
             sgj_pr_hr(jsp, " Inquiry response length=%d, no vendor, product "
-                      "or revision data\n", op->maxlen);
+                      "or revision data\n", len);
     } else {
         int i;
 
@@ -2213,7 +2222,7 @@ std_inq_decode(struct opts_t * op, sgj_opaque_p jop, int off)
             }
         } else
             sgj_pr_hr(jsp, " Vendor identification: %s\n", xtra_buff);
-        if (op->maxlen <= 16) {
+        if (len <= 16) {
             if (! op->do_export)
                 sgj_pr_hr(jsp, " Product identification: <none>\n");
         } else {
@@ -2229,7 +2238,7 @@ std_inq_decode(struct opts_t * op, sgj_opaque_p jop, int off)
             } else
                 sgj_pr_hr(jsp, " Product identification: %s\n", xtra_buff);
         }
-        if (op->maxlen <= 32) {
+        if (len <= 32) {
             if (! op->do_export)
                 sgj_pr_hr(jsp, " Product revision level: <none>\n");
         } else {
@@ -2242,10 +2251,9 @@ std_inq_decode(struct opts_t * op, sgj_opaque_p jop, int off)
             } else
                 sgj_pr_hr(jsp, " Product revision level: %s\n", xtra_buff);
         }
-        if (op->do_vendor && (op->maxlen > 36) && ('\0' != rp[36]) &&
+        if (op->do_vendor && (len > 36) && ('\0' != rp[36]) &&
             (' ' != rp[36])) {
-            memcpy(xtra_buff, &rp[36], op->maxlen < 56 ? op->maxlen - 36 :
-                   20);
+            memcpy(xtra_buff, &rp[36], len < 56 ? len - 36 : 20);
             if (op->do_export) {
                 len = encode_whitespaces((uint8_t *)xtra_buff, 20);
                 if (len > 0)
@@ -2254,21 +2262,21 @@ std_inq_decode(struct opts_t * op, sgj_opaque_p jop, int off)
                 sgj_pr_hr(jsp, " Vendor specific: %s\n", xtra_buff);
         }
         if (op->do_descriptors) {
-            for (j = 0, k = 58; ((j < 8) && ((k + 1) < op->maxlen));
+            for (j = 0, k = 58; ((j < 8) && ((k + 1) < len));
                  k +=2, ++j)
                 vdesc_arr[j] = sg_get_unaligned_be16(rp + k);
         }
-        if ((op->do_vendor > 1) && (op->maxlen > 96)) {
-            memcpy(xtra_buff, &rp[96], op->maxlen - 96);
+        if ((op->do_vendor > 1) && (len > 96)) {
+            memcpy(xtra_buff, &rp[96], len - 96);
             if (op->do_export) {
                 len = encode_whitespaces((uint8_t *)xtra_buff,
-                                         op->maxlen - 96);
+                                         len - 96);
                 if (len > 0)
                     printf("VENDOR_SPECIFIC=%s\n", xtra_buff);
             } else
                 sgj_pr_hr(jsp, " Vendor specific: %s\n", xtra_buff);
         }
-        if (op->do_vendor && (op->maxlen > 243) &&
+        if (op->do_vendor && (len > 243) &&
             (0 == strncmp("OPEN-V", (const char *)&rp[16], 6))) {
            memcpy(xtra_buff, &rp[212], 32);
            if (op->do_export) {
@@ -2284,8 +2292,8 @@ std_inq_decode(struct opts_t * op, sgj_opaque_p jop, int off)
         sgj_opaque_p jo2p = NULL;
 
         if (as_json)
-            jo2p = std_inq_decode_js(rp, op->maxlen, op, jop);
-        if ((0 == op->maxlen) && usn_buff[0])
+            jo2p = std_inq_decode_js(rp, len, op, jop);
+        if ((0 == len) && usn_buff[0])
             sgj_pr_hr(jsp, " Unit serial number: %s\n", usn_buff);
         if (op->do_descriptors) {
             sgj_opaque_p jap = sgj_named_subarray_r(jsp, jo2p,
@@ -2388,12 +2396,12 @@ std_inq_process(int sg_fd, struct opts_t * op, sgj_opaque_p jop, int off)
     int vb, resid;
     char buff[48];
 
-    if (sg_fd < 0) {    /* assume --inhex=FD usage */
-        std_inq_decode(op, jop, off);
-        return 0;
-    }
     rlen = (op->maxlen > 0) ? op->maxlen : SAFE_STD_INQ_RESP_LEN;
     vb = op->verbose;
+    if (sg_fd < 0) {    /* assume --inhex=FD usage */
+        std_inq_decode(rsp_buff + off, rlen, op, jop);
+        return 0;
+    }
     res = sg_ll_inquiry_v2(sg_fd, false, 0, rsp_buff, rlen, DEF_PT_TIMEOUT,
                            &resid, false, vb);
     if (0 == res) {
@@ -2430,8 +2438,7 @@ std_inq_process(int sg_fd, struct opts_t * op, sgj_opaque_p jop, int off)
             if (fetch_unit_serial_num(sg_fd, usn_buff, sizeof(usn_buff), vb))
                 usn_buff[0] = '\0';
         }
-        op->maxlen = act_len;
-        std_inq_decode(op, jop, 0);
+        std_inq_decode(rsp_buff, act_len, op, jop);
         return 0;
     } else if (res < 0) { /* could be an ATA device */
 #if defined(SG_LIB_LINUX) && defined(SG_SCSI_STRINGS) && \
@@ -2862,7 +2869,9 @@ vpd_decode(int sg_fd, struct opts_t * op, sgj_opaque_p jop, int off)
             bool protect = false;
 
             op->protect_not_sure = false;
-            if ((sg_fd >= 0) && (! op->do_force)) {
+            if (op->std_inq_a_valid)
+                 protect = !! (0x1 & op->std_inq_a[5]);
+            else if ((sg_fd >= 0) && (! op->do_force)) {
                 struct sg_simple_inquiry_resp sir;
 
                 res = sg_simple_inquiry(sg_fd, &sir, false, vb);
@@ -4293,6 +4302,7 @@ main(int argc, char * argv[])
             ret = SG_LIB_CONTRADICT;
             goto err_out;
         }
+        /* Note: want to support both --sinq_inraw= and --inhex= options */
         if ((ret = sg_f2hex_arr(op->sinq_inraw_fn, true, false, rsp_buff,
                                 &inraw_len, rsp_buff_sz))) {
             goto err_out;
@@ -4370,9 +4380,9 @@ main(int argc, char * argv[])
             ret = svpd_inhex_decode_all(op, jop);
             goto fini2;
         }
-    } else if (0 == op->device_name) {
+    } else if ((NULL == op->device_name) && (! op->std_inq_a_valid)) {
         pr2serr("No DEVICE argument given\n\n");
-        usage_for(op);
+        pr2serr("Use '-h' or '--help' option for usage summary\n");
         ret = SG_LIB_SYNTAX_ERROR;
         goto err_out;
     }
@@ -4472,6 +4482,18 @@ main(int argc, char * argv[])
             ret = std_inq_process(-1, op, jop, 0);
             goto err_out;
         }
+    } else if (op->std_inq_a_valid && (NULL == op->device_name)) {
+        /* --sinq_inraw=RFN contents still in rsp_buff */
+        if (op->do_raw)
+            dStrRaw((const char *)rsp_buff, inraw_len);
+        else if (op->do_hex) {
+            if (! op->do_quiet && (op->do_hex < 3))
+                sgj_pr_hr(jsp, "Standard Inquiry data format:\n");
+            hex2stdout(rsp_buff, inraw_len, (1 == op->do_hex) ? 0 : -1);
+        } else
+            std_inq_decode(rsp_buff, inraw_len, op, jop);
+        ret = 0;
+        goto fini2;
     }
 
 #if defined(O_NONBLOCK) && defined(O_RDONLY)

COVERAGE
@@ -7,103 +7,103 @@ third table list supported NVMe commands.
 
 SCSI command        sg3_utils utilities that use this SCSI command
 ------------        -------------------------------------------------
-ATA COMMAND PASS-THROUGH(12)  sg_sat_identify, ++
+ATA COMMAND PASS-THROUGH(12)  sg_sat_identify
 ATA COMMAND PASS-THROUGH(16)  sg_sat_identify, sg_sat_set_features,
-                    sg_sat_phy_event, sg_sat_read_gplog ++
+                    sg_sat_phy_event, sg_sat_read_gplog
                     [sg_sat_chk_power, sg__sat_identify,
                      sg__sat_set_features, sg_sat_smart_rd_data
                      (previous four in the examples directory)]
-ATA COMMAND PASS-THROUGH(32)  sg_sat_identify, ++
+ATA COMMAND PASS-THROUGH(32)  sg_sat_identify
 BACKGROUND CONTROL  sg_bg_ctl
 CLOSE ZONE          sg_zone
 COMPARE AND WRITE   sg_compare_and_write
-COPY OPERATION ABORT    ddptctl, ++
-EXTENDED COPY(LID1)    sg_xcopy, ddpt, ++
-GET CONFIGURATION   sg_get_config, ++
-GET LBA STATUS      sg_get_lba_status, ++ <J>
-GET PHYSICAL ELEMENT STATUS      sg_get_elem_status, ++ <J>
+COPY OPERATION ABORT    ddptctl
+EXTENDED COPY(LID1)    sg_xcopy, ddpt
+GET CONFIGURATION   sg_get_config
+GET LBA STATUS      sg_get_lba_status <J>
+GET PHYSICAL ELEMENT STATUS      sg_get_elem_status <J>
 GET STREAM STATUS   sg_stream_ctl
-INQUIRY             sg_dd, sg_format, sg_inq, sginfo,
-                    sg_logs, sg_map('-i'), sg_modes, sg_opcodes,
-                    sg_persist, sg_scan, sg_ses, sg_vpd ++ <J>
+INQUIRY             sg_dd, sg_format, sg_inq, sginfo, sg_logs,
+                    sg_luns, sg_map('-i'), sg_modes, sg_opcodes,
+                    sg_persist, sg_scan, sg_ses, sg_vpd <J>
 FINISH ZONE         sg_zone
-FORMAT MEDIUM       sg_format, ++ [SSC]
-FORMAT UNIT         sg_format, ++ [SBC]
-FORMAT WITH PRESET    sg_format, ++ [SBC]
-LOG SELECT          sg_logs('-r' or '-select'), ++
-LOG SENSE           sg_logs, ++ <J>
+FORMAT MEDIUM       sg_format, [SSC]
+FORMAT UNIT         sg_format, [SBC]
+FORMAT WITH PRESET    sg_format, [SBC]
+LOG SELECT          sg_logs('-r' or '-select')
+LOG SENSE           sg_logs <J>
 MODE SELECT(6)      sdparm, sg_wr_mode, sginfo, sg_format,
-                    sg_emc_trespass, sg_rdac, ++
+                    sg_emc_trespass, sg_rdac
 MODE SELECT(10)     sdparm, sg_wr_mode, sginfo, sg_format,
-                    sg_emc_trespass, sg_rdac, ++
+                    sg_emc_trespass, sg_rdac
 MODE SENSE(6)       sdparm, sg_modes, sg_wr_mode, sginfo, sg_format,
-                    sg_senddiag('-e'), sg_rdac, ++
+                    sg_senddiag('-e'), sg_rdac
 MODE SENSE(10)      sdparm, sg_modes, sg_wr_mode, sginfo, sg_format,
-                    sg_senddiag('-e'), sg_rdac, ++
+                    sg_senddiag('-e'), sg_rdac
 OPEN ZONE           sg_zone
 ORWRITE(16)         sg_write_x
 ORWRITE(32)         sg_write_x
-PERSISTENT RESERVE IN       sg_persist, ++
-PERSISTENT RESERVE OUT      sg_persist, ++
-POPULATE TOKEN      ddpt, ddptctl, ++
+PERSISTENT RESERVE IN       sg_persist
+PERSISTENT RESERVE OUT      sg_persist
+POPULATE TOKEN      ddpt, ddptctl
 PRE-FETCH(10)       sg_seek
 PRE-FETCH(16)       sg_seek
-PREVENT ALLOW MEDIUM REMOVAL        sg_prevent, ++
+PREVENT ALLOW MEDIUM REMOVAL        sg_prevent
 READ(6)             sg_dd, sgm_dd, sgp_dd, sg_read
 READ(10)            sg_dd, sgm_dd, sgp_dd, sg_read
 READ(12)            sg_dd, sgm_dd, sgp_dd, sg_read
 READ(16)            sg_dd, sgm_dd, sgp_dd, sg_read
 READ ATTRIBUTE      sg_read_attr
-READ BLOCK LIMITS   sg_read_block_limits, ++
-READ BUFFER(10)     sg_rbuf, sg_test_rwbuf, sg_read_buffer, sg_safte, ++
+READ BLOCK LIMITS   sg_read_block_limits
+READ BUFFER(10)     sg_rbuf, sg_test_rwbuf, sg_read_buffer, sg_safte
 READ BUFFER(16)     sg_read_buffer
-READ CAPACITY(10)   sg_readcap, sg_dd, sgm_dd, sgp_dd, sg_format, ++
-READ CAPACITY(16)   sg_readcap, sg_dd, sgm_dd, sgp_dd, sg_format, ++
-READ DEFECT(10)     sginfo('-d' or '-G'), sg_reassign('-g'), smartmontools, ++
+READ CAPACITY(10)   sg_readcap <J>, sg_dd, sgm_dd, sgp_dd, sg_format
+READ CAPACITY(16)   sg_readcap <J>, sg_dd, sgm_dd, sgp_dd, sg_format
+READ DEFECT(10)     sginfo('-d' or '-G'), sg_reassign('-g'), smartmontools
 READ DEFECT(12)     sginfo('-d' or '-G'), smartmontools
-READ LONG(10)       sg_read_long, sg_dd, ++
-READ LONG(16)       sg_read_long, ++
-READ MEDIA SERIAL NUMBER     sg_rmsn, ++
-REASSIGN BLOCKS     sg_reassign, ++
-RECEIVE COPY DATA(LID1)    sg_copy_results, ++
-RECEIVE COPY FAILURE DETAILS(LID1)    sg_copy_results, ++
-RECEIVE COPY OPERATING PARAMETERS    ddpt, sg_copy_results, sg_xcopy, ++
-RECEIVE COPY STATUS(LID1)    sg_copy_results, ++
-RECEIVE DIAGNOSTIC RESULTS  sg_senddiag, sg_ses <J>, sg_ses_microcode ++
-RECEIVE ROD TOKEN INFORMATION    ddpt, ddptctl ++
+READ LONG(10)       sg_read_long, sg_dd
+READ LONG(16)       sg_read_long
+READ MEDIA SERIAL NUMBER     sg_rmsn
+REASSIGN BLOCKS     sg_reassign
+RECEIVE COPY DATA(LID1)    sg_copy_results
+RECEIVE COPY FAILURE DETAILS(LID1)    sg_copy_results
+RECEIVE COPY OPERATING PARAMETERS    ddpt, sg_copy_results, sg_xcopy
+RECEIVE COPY STATUS(LID1)    sg_copy_results
+RECEIVE DIAGNOSTIC RESULTS  sg_senddiag, sg_ses <J>, sg_ses_microcode
+RECEIVE ROD TOKEN INFORMATION    ddpt, ddptctl
 REMOVE ELEMENT AND MODIFY ZONES   sg_zone
 REMOVE ELEMENT AND TRUNCATE       sg_rem_rest_elem
-REPORT ALL ROD TOKENS    ddptctl ++
+REPORT ALL ROD TOKENS    ddptctl
 REPORT DENSITY SUPPORT    sg_rep_density
-REPORT IDENTIFYING INFORMATION  sg_ident, ++ (2)
-REPORT LUNS         sg_luns, ++
-REPORT PROVISIONING INITIALIZATION PATTERN    sg_rep_pip, ++
+REPORT IDENTIFYING INFORMATION  sg_ident (2)
+REPORT LUNS         sg_luns <J>
+REPORT PROVISIONING INITIALIZATION PATTERN    sg_rep_pip
 REPORT REALMS       sg_rep_zones <J>
-REPORT REFERRALS    sg_referrals, ++
+REPORT REFERRALS    sg_referrals
 REPORT SUPPORTED OPERATION CODES              sg_opcodes <J>
 REPORT SUPPORTED TASK MANAGEMENT FUNCTIONS    sg_opcodes <J>
-REPORT TARGET PORT GROUPS       sg_rtpg, sg_stpg ++
+REPORT TARGET PORT GROUPS       sg_rtpg, sg_stpg
 REPORT TIMESTAMP    sg_timestamp
 REPORT ZONES        sg_rep_zones <J>
 REPORT ZONE DOMAINS  sg_rep_zones <J>
-REQUEST SENSE       sg_requests, ++
+REQUEST SENSE       sg_requests
 RESET WRITE POINTER sg_reset_wp
 RESTORE ELEMENTS AND REBUILD    sg_rem_rest_elem
 SANITIZE            sg_sanitize
-SEEK(10)            sg_seek ++
-SEND DIAGNOSTIC     sg_senddiag, sg_ses <J>, sg_ses_microcode ++
+SEEK(10)            sg_seek
+SEND DIAGNOSTIC     sg_senddiag, sg_ses <J>, sg_ses_microcode
 SEQUENTIALIZE ZONE  sg_zone
-SET IDENTIFYING INFORMATION  sg_ident, ++ (3)
-SET TARGET PORT GROUPS       sg_stpg, ++
+SET IDENTIFYING INFORMATION  sg_ident (3)
+SET TARGET PORT GROUPS       sg_stpg
 SET TIMESTAMP       sg_timestamp
-START STOP          sg_start, ++
+START STOP          sg_start
 STREAM CONTROL      sg_stream_ctl
-SYNCHRONIZE CACHE(10)   sg_sync, sg_dd, sgm_dd, sgp_dd, ++
-SYNCHRONIZE CACHE(16)   sg_sync++
-TEST UNIT READY     sg_turs, sg_format, ++
-UNMAP               sg_unmap, ++
-VERIFY(10)          sg_verify, ++
-VERIFY(16)          sg_verify, ++
+SYNCHRONIZE CACHE(10)   sg_sync, sg_dd, sgm_dd, sgp_dd
+SYNCHRONIZE CACHE(16)   sg_sync
+TEST UNIT READY     sg_turs, sg_format
+UNMAP               sg_unmap
+VERIFY(10)          sg_verify
+VERIFY(16)          sg_verify
 WRITE(6)            sg_dd, sgm_dd, sgp_dd
 WRITE(10)           sg_dd, sgm_dd, sgp_dd
 WRITE(12)           sg_dd, sgm_dd, sgp_dd
@@ -114,9 +114,9 @@ WRITE AND VERIFY(16)      sg_write_verify
 WRITE ATOMIC(16)    ddpt, sg_write_x
 WRITE ATOMIC(32)    sg_write_x
 WRITE ATTRIBUTE     sg_write_attr
-WRITE BUFFER        sg_test_rwbuf, sg_write_buffer, ++
-WRITE LONG(10)      sg_write_long, ++
-WRITE LONG(16)      sg_write_long, ++
+WRITE BUFFER        sg_test_rwbuf, sg_write_buffer
+WRITE LONG(10)      sg_write_long
+WRITE LONG(16)      sg_write_long
 WRITE SAME(10)      sg_write_same
 WRITE SAME(16)      sg_write_same, sg_write_x
 WRITE SAME(32)      sg_write_same, sg_write_x
@@ -124,7 +124,7 @@ WRITE SCATTERED(16)    sg_write_x
 WRITE SCATTERED(32)    sg_write_x
 WRITE STREAM(16)    sg_write_x
 WRITE STREAM(32)    sg_write_x
-WRITE USING TOKEN   ddpt, ddptctl, ++
+WRITE USING TOKEN   ddpt, ddptctl
 ZONE ACTIVATE       sg_z_act_query
 ZONE QUERY          sg_z_act_query
 <all commands>      sg_decodes_sense <J>
@@ -171,8 +171,6 @@ and REQUEST SENSE. READ CAPACITY(10 and 16) yield appropriate data
 by examining the response to the NVMe Identify command.
 
 
-++  command wrapper found in sg_cmds_basic.c, sg_cmds_mmc.c  or
-    sg_cmds_extra.c for this command
 (2) this command was known as REPORT DEVICE IDENTIFIER prior to spc4r07
 (3) this command was known as SET DEVICE IDENTIFIER prior to spc4r07
 <J> decoded results may be output in JSON instead of human readable form
@@ -189,4 +187,4 @@ THIRD PARTY COPY IN (0x83).
 
 
 Douglas Gilbert
-06 January 2023
+04 February 2023

doc/sg3_utils.8
@@ -1,4 +1,4 @@
-.TH SG3_UTILS "8" "January 2023" "sg3_utils\-1.48" SG3_UTILS
+.TH SG3_UTILS "8" "February 2023" "sg3_utils\-1.48" SG3_UTILS
 .SH NAME
 sg3_utils \- a package of utilities for sending SCSI commands
 .SH SYNOPSIS
@@ -608,12 +608,12 @@ appended). To produce hexadecimal that can be parsed by other utilities use
 this option three, four or five times.
 .br
 The use of this option, especially when used multiple times has evolved over
-time as it has become more powerful. So check this option's description in
-the manpage of the utility being used. An example of a corner case with
-the '\-HHH' option is with the sg_inq utility and the ATA Information VPD
-page. The sg_inq utility does not decode the ATA IDENTIFY DEVICE command
-response but instead outputs hexadecimal 16 bit works (little endian)
-suitable for the hdparm utility to decode.
+time as it has become more useful (at least in testing and fixing bugs). So
+check this option's description in the manpage of the utility being used. An
+example of a corner case with the '\-HHH' option is with the sg_inq utility
+and the ATA Information VPD page. The sg_inq utility does not decode the
+ATA IDENTIFY DEVICE command response but instead outputs hexadecimal 16 bit
+words (little endian) suitable for the hdparm utility to decode.
 .TP
 \fB\-i\fR, \fB\-\-in\fR=\fIFN\fR
 many SCSI commands fetch a significant amount of data (returned in the

doc/sg_inq.8
@@ -1,4 +1,4 @@
-.TH SG_INQ "8" "December 2022" "sg3_utils\-1.48" SG3_UTILS
+.TH SG_INQ "8" "February 2023" "sg3_utils\-1.48" SG3_UTILS
 .SH NAME
 sg_inq \- issue SCSI INQUIRY command and/or decode its response
 .SH SYNOPSIS
@@ -8,8 +8,9 @@ sg_inq \- issue SCSI INQUIRY command and/or decode its response
 [\fI\-\-force\fR] [\fI\-\-help\fR] [\fI\-\-hex\fR] [\fI\-\-id\fR]
 [\fI\-\-inhex=FN\fR] [\fI\-\-json[=JO]\fR] [\fI\-\-js\-file=JFN\fR]
 [\fI\-\-len=LEN\fR] [\fI\-\-long\fR] [\fI\-\-maxlen=LEN\fR]
-[\fI\-\-only\fR] [\fI\-\-page=PG\fR] [\fI\-\-raw\fR] [\fI\-\-vendor\fR]
-[\fI\-\-verbose\fR] [\fI\-\-version\fR] [\fI\-\-vpd\fR] \fIDEVICE\fR
+[\fI\-\-only\fR] [\fI\-\-page=PG\fR] [\fI\-\-raw\fR]
+[\fI\-\fI\-sinq_inraw=RFN\fR] [\fI\-\-vendor\fR] [\fI\-\-verbose\fR]
+[\fI\-\-version\fR] [\fI\-\-vpd\fR] \fIDEVICE\fR
 .PP
 .B sg_inq
 [\fI\-36\fR] [\fI\-a\fR] [\fI\-A\fR] [\fI\-b\fR] [\fI\-\-B=0|1\fR]
@@ -237,6 +238,25 @@ used. The binary is sent to stdout, and errors are sent to stderr.
 .br
 If used with \fI\-\-inhex=FN\fR then the contents of \fIFN\fR is treated as
 binary.
+.TP
+\fB\-Q\fR, \fB\-\-sinq_inraw\fR=\fIRFN\fR
+where \fIRFN\fR is a filename containing binary standard INQUIRY response
+data that matches either \fIDEVICE\fR or \fIFN\fR. Linux places this standard
+INQUIRY response in its sysfs pseudo filesystem. A typical location is at
+/sys/class/scsi_device/<hctl>/device/inquiry where <hctl> is a four part
+numeric tuple separated by colons. This tuple distinguishes the device from
+any others on the system. Linux also places some VPD page responses in binary
+in the same directory with names like "vpd_pg83" where the last two digits
+form the hexadecimal VPD page number whose binary contents are therein.
+.br
+Some VPD pages (e.g. the Extended Inquiry VPD page) depend on knowing the
+settings in the standard INQUIRY response to interpret the fields in that VPD
+page. This option together with the \fI\-\-all\fR, \fI\-\-examine\fR or
+\fI\-\-page=PG\fR allows this utility to process both the standard INQUIRY
+response and VPD pages in the same invocation.
+.br
+The \fI\-\-raw\fR option has no effect on this option. The \fIDEVICE\fR
+argument may be given with this option.
 .TP
 \fB\-s\fR, \fB\-\-vendor\fR
 output a standard INQUIRY response's vendor specific fields from offset 36
@@ -560,11 +580,11 @@ Written by Douglas Gilbert
 .SH "REPORTING BUGS"
 Report bugs to <dgilbert at interlog dot com>.
 .SH COPYRIGHT
-Copyright \(co 2001\-2022 Douglas Gilbert
+Copyright \(co 2001\-2023 Douglas Gilbert
 .br
 This software is distributed under the GPL version 2 or the BSD\-2\-Clause
 license. There is NO warranty; not even for MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE.
 .SH "SEE ALSO"
-.B sg_opcodes(8), sg_vpd(8), sg_logs(8), sg_modes(8), sdparm(8), hdparm(8),
-.B sgdiag(scsirastools)
+.B sg_opcodes(8), sg_vpd(8), sg_logs(8), sg_modes(8), sg_decode_sense(8),
+.B sdparm(8), hdparm(8),sgdiag(scsirastools)

commit 42fd788049250694630f7cd833347a798063d360

sg_inq: re-add Unit serial number field
After 0f1230a ("sg_readcap+sg_luns: add --inhex= and json"), the
"Unit serial number" output field is missing from sg_inq output:

> ./sg_inq /dev/sda
standard INQUIRY:
  PQual=0  PDT=0  RMB=0  LU_CONG=0  hot_pluggable=0  version=0x05  [SPC-3]
  [AERC=0]  [TrmTsk=0]  NormACA=0  HiSUP=0  Resp_data_format=2
  SCCS=0  ACC=0  TPGS=0  3PC=0  Protect=0  [BQue=0]
  EncServ=0  MultiP=0  [MChngr=0]  [ACKREQQ=0]  Addr16=0
  [RelAdr=0]  WBus16=0  Sync=0  [Linked=0]  [TranDis=0]  CmdQue=1
  [SPI: Clocking=0x0  QAS=0  IUS=0]
    length=96 (0x60)   Peripheral device type: disk
  Vendor identification: ATA
  Product identification: SK hynix SC300 M
  Product revision level: 0P00

Previously, the command would also print

  Unit serial number: FJ63N456110303C1E

The problem is caused by checking "len" rather than op->maxlen
in the condition that's evaluated for printing the unit serial numnber.

Fixes: 0f1230a ("sg_readcap+sg_luns: add --inhex= and json")
Signed-off-by: Martin Wilck <mwilck@suse.com>

src/sg_inq.c
@@ -2450,7 +2450,7 @@ std_inq_decode(const uint8_t * rp, int len, struct opts_t * op,
 
         if (as_json)
             jo2p = std_inq_decode_js(rp, len, op, jop);
-        if ((0 == len) && usn_buff[0])
+        if ((0 == op->maxlen) && usn_buff[0])
             sgj_pr_hr(jsp, "  Unit serial number: %s\n", usn_buff);
         if (op->do_descriptors) {
             sgj_opaque_p jap = sgj_named_subarray_r(jsp, jo2p,